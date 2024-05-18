b51c92e8602d8d5b600e1cdd68395cca0fc7c7fc
Fred Sauer <fredsa@google.com>
2024-05-17 19:16:58 -0700

    remove unncessary `await`, fix semicolon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -87,13 +87,13 @@ export class AppComponent {
     const model = genAI.getGenerativeModel({
       model: 'gemini-1.5-pro-latest'
     });
-    model.generationConfig.responseMimeType = 'application/json'
+    model.generationConfig.responseMimeType = 'application/json';
     
     this.loading = true;
 
     try {
       const result = await model.generateContent(prompt);
-      const response = await result.response;
+      const response = result.response;
       const text = response.text();
       const tune = JSON.parse(text);
 
